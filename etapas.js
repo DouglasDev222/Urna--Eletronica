6bb131ead417ca428b9e886e42e5f6d9951ab097
Douglas Santos <douglasdev222@gmail.com>
2022-09-19 15:15:00 -0300

    Primeiro commit

---
+++ etapas.js
@@ -0,0 +1,54 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "11011",
+        nome: "Lenilson do Sopão",
+        partido: "PL",
+        fotos: [{ url: "lenilson.jpg", legenda: "Vereador" }],
+      },
+      {
+        numero: "12012",
+        nome: "Edilene do AEE",
+        partido: "AEE",
+        fotos: [{ url: "edilene.jpg", legenda: "Vereador" }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "27",
+        nome: "Douglas da Micoca",
+        partido: "TPD",
+        vice: "Geovanna da Nordece",
+        fotos: [
+          { url: "Douglas.jpg", legenda: "Prefeito" },
+          {
+            url: "Geo.jpg",
+            legenda: "Vice-Prefeito",
+            small: true,
+          },
+        ],
+      },
+      {
+        numero: "69",
+        nome: "Amois da Construção",
+        partido: "CDP",
+        vice: "Kevinho da Prata",
+        fotos: [
+          { url: "amois.jpg", legenda: "Prefeito" },
+          {
+            url: "kevinho.jpg",
+            legenda: "Vice-Prefeito",
+            small: true,
+          },
+        ],
+      },
+    ],
+  },
+];
